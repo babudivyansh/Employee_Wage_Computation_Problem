9f6fd8894ff015a2608f53a217821ba08e012ad0
babudivyansh <divyansh.verma525@gmail.com>
2023-11-29 14:12:36 +0530

    [Divyansh Babu] add:Logic to calculate the daily wage of an employee

--- emp.py
+++ emp.py
@@ -23,11 +23,16 @@ def check_attendence():
     print("Welcome to Employee Wage Computation Program on Master Branch")
 
     FULL_TIME = 1
+    WAGE_PER_HR = 20
     empType = random.randint(0, 100) % 2
+    workingHours = 0
     if empType == FULL_TIME:
         print("Employee is present")
+        workingHours = 8
     else:
         print("Employee is not present")
+    wage = workingHours * WAGE_PER_HR
+    print("Employee Daily Wage is: ", wage)
 
 
 if __name__ == '__main__':
