958bb1f99e042c02d636063e464c7386de6d8e9a
babudivyansh <divyansh.verma525@gmail.com>
2023-11-29 15:45:45 +0530

    [Divyansh Babu] add: Implementation using match case.

--- emp.py
+++ emp.py
@@ -1,11 +1,11 @@
 """
 @Author: Divyansh Babu
 
-@Date: 2021-11-28 13:06
+@Date: 2021-11-29 15:06
 
 @Last Modified by: Divyansh Babu
 
-@Last Modified time: 2021-11-28 13:06
+@Last Modified time: 2021-11-29 15:06
 
 @Title : Employee Wage Computation Problem.
 """
@@ -24,16 +24,17 @@ def check_attendence():
     PART_TIME = 1
     FULL_TIME = 2
     WAGE_PER_HR = 20
-    empType = random.randint(0, 100) % 3
+    empType = random.randint(0, 2)
     workingHours = 0
-    if empType == FULL_TIME:
-        print("Employee is present Full time")
-        workingHours = 8
-    elif empType == PART_TIME:
-        print("Employee is working Part time")
-        workingHours = 4
-    else:
-        print("Employee is not present")
+    match empType:
+        case 0:
+            print("Employee is present Full time")
+            workingHours = 8
+        case 1:
+            print("Employee is working Part time")
+            workingHours = 4
+        case 2:
+            print("Employee is not present")
     wage = workingHours * WAGE_PER_HR
     print("Employee Daily Wage is: ", wage)
 
